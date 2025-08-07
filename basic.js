ae4a64140f4e88f85e88e11554425a1af44c5fc5
Badsha012 <badshasheikh361@gmail.com>
2025-08-07 08:43:15 -0700

    six commit

---
+++ basic.js
@@ -0,0 +1,15 @@
+const num=45;
+function funoff(){
+    console.log('bosia thaka utia dira');
+    console.log('click the switch off kor');
+
+}
+
+// function name (){}
+
+function bruchtaech(){
+   console.log('pcup the bruch');
+   console.log('two minutes ghoso')
+}
+//function---call
+funoff();
