07e0824c78b12ad050cb75e5315f147a1762eede
Badsha012 <badshasheikh361@gmail.com>
2025-08-07 10:01:21 -0700

    four commit

---
+++ parameter.js
@@ -0,0 +1,21 @@
+function vatkow(params) {
+    console.log('handwash koro');
+    console.log('boso');
+
+    
+}
+// vatkow();
+
+
+
+
+//squre root
+
+function squre(number) {
+
+
+    console.log('squre--->' ,number);
+    const n=number*number;
+    console.log('squ number-->',n);
+}
+squre(7);
