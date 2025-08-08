d8048aba4990a1f075a38b9944923bd3ca743b5a
Badsha012 <badshasheikh361@gmail.com>
2025-08-07 20:00:28 -0700

    seven commit

---
+++ aru.js
@@ -0,0 +1,16 @@
+function double(n) {
+    const double=n*2;
+    console.log(double);
+    
+}
+console.log('I will call the function');
+double(5);
+double(888);
+
+
+function diff(a,b) {
+    const z=a-b;
+    console.log(z);
+    
+}
+diff(55,6);
---
+++ return.js
@@ -0,0 +1,17 @@
+
+
+function tentimes(m) {
+    const resut=m*10;
+    return resut;
+    
+}
+function cutHalf(number){
+    const half=number/2;
+    return half;
+
+}
+tentimes(5);
+const t=tentimes(5);
+console.log(t);
+
+
