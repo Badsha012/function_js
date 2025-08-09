d69326fca6244ec2ce0f4fcd53d66a9079602c8c
Badsha012 <badshasheikh361@gmail.com>
2025-08-09 10:06:55 -0700

    six commit

---
+++ date.js
@@ -0,0 +1,12 @@
+const today = new Date();
+const data= new Date ('1971-10-09')
+console.log(data.getMonth());
+console.log(data.getDay());
+const specificDate = new Date(2091,0,26)
+console.log(specificDate);
+specificDate.setMonth(10);
+console.log(specificDate.toLocaleString('en-GB'));
+
+
+
+// unix epoc 
