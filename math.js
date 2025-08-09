ed9e5310c3f4a3f676b2b5b6131e11096a298b73
Badsha012 <badshasheikh361@gmail.com>
2025-08-09 09:41:57 -0700

    five commit

---
+++ math.js
@@ -0,0 +1,32 @@
+
+
+// const Math={
+//     min:function min(x,y){
+//         return y;
+//     }
+
+// }
+// console.log(Math.min);
+
+// const min=Math.min
+
+
+const min=Math.min(45,44,32,21);
+console.log(min);
+
+
+
+const max=Math.max(45,44,32,21);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-88));
+console.log(Math.round(2.85));
+console.log(Math.round(2.12));
+console.log(Math.floor(3.99));
+console.log(Math.ceil(4.22));
+console.log(Math.random());
+
+//const rand=Math.random()*10;
+const rand=Math.round(Math.random()*10)
+console.log(rand);
