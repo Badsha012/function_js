1fc27f63315cbd51cf2c70f09cd76376d515a687
Badsha012 <badshasheikh361@gmail.com>
2025-08-09 08:37:19 -0700

    seven commit

--- leap_year.js
+++ leap_year.js
@@ -19,8 +19,13 @@ function isLeapyear(year) {
     if(year % 100 !==0 && year % 4===0){
         return true;
     }
+    if(year % 100===0 && year % 400 ===0){
+        return true;
+    }
     else{
         return false;
     }
     
 }
+const islipi=isLeapyear(2052);
+console.log(islipi);
