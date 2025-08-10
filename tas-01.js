14332a9a4c0e71683b6d2df1abc80f0fdd93b716
Badsha012 <badshasheikh361@gmail.com>
2025-08-09 18:08:20 -0700

    nine commit

---
+++ tas-01.js
@@ -0,0 +1,25 @@
+
+
+
+//convet into clcius to fahnheit
+function celciusTofahnheit(c) {
+
+const f=c*9/5+32;
+return f;
+
+
+    
+}
+const temputure=celciusTofahnheit(56);
+console.log(temputure);
+
+
+
+// f to c
+function isFCelcius(temp) {
+    const celi=temp-32 *5/9;
+    return celi;
+    
+}
+const cel=isFCelcius(40);
+console.log(cel);
---
+++ temp.js
@@ -0,0 +1,18 @@
+let a=6;
+let n=9;
+console.log(a,n);
+ 
+ const temp=a;
+a=n;
+n=temp;
+console.log(a,n);
+
+
+
+
+let x=7;
+let y=3;
+
+console.log(x,y);
+[x,y]=[y,x];
+console.log(x,y);
