6c92617173081e88c8227f1444a33860b56422e8
Badsha012 <badshasheikh361@gmail.com>
2025-08-10 07:38:13 -0700

    ten commit

---
+++ max.js
@@ -0,0 +1,23 @@
+
+const disha= 56;
+const mim=95;
+if(disha>mim){
+    console.log('Disha will get the strawberry');
+}
+else{
+    console.log('Mim will get the strawberry');
+}
+
+//insde a function
+
+function isMax(num1,num2) {
+    if(num1>num2){
+        return num1;
+    }
+    else{
+        return num2;
+    }
+    
+}
+const max=isMax(56,97);
+console.log('max of two:',max);
---
+++ max3.js
@@ -0,0 +1,16 @@
+const jim=56;
+const tim=89;
+const kim=92;
+if(jim>tim &&  jim>kim){
+    console.log("Jim ultimate boss");
+
+}
+else if(tim>jim && tim>kim){
+    console.log('Tim is boss');
+
+}
+else{
+    console.log('Kim is kardash boss');
+}
+
+//function
