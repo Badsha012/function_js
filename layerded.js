925389c6c9984deb9d52dbcc38dbfbc8be512217
Badsha012 <badshasheikh361@gmail.com>
2025-08-11 07:38:34 -0700

    five commit

---
+++ layerded.js
@@ -0,0 +1,17 @@
+function layerDis(quenty) {
+    const f=100;
+    const s=90;
+    const r=70;
+    if(quenty=100){
+        const t=quenty*f;
+        return t;
+
+    }
+    else if(quenty<=200){
+        const f=100*f;
+        const p=quenty-f;
+        const x=s*p;
+        const v=f;
+    }
+    
+}
